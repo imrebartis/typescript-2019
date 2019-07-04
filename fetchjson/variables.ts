fac0f39de339ee092b48dda4bbbf05a6282deacd
Imre Bartis <imre.bartis@siili.com>
2019-07-04 10:48:51 +0300

    fix the 'any' type

--- fetchjson/variables.ts
+++ fetchjson/variables.ts
@@ -1,5 +1,5 @@
 let apples: number = 5;
-let speed: string = 'fast';
+let speed: string = "fast";
 let hasName: boolean = true;
 let nothingMuch: null = null;
 let nothing: undefined = undefined;
@@ -8,14 +8,12 @@ let nothing: undefined = undefined;
 let now: Date = new Date();
 
 // Arrays
-let colors: string[] = ['red', 'green', 'blue'];
-let myNumbers: number[] = [1,2,3];
+let colors: string[] = ["red", "green", "blue"];
+let myNumbers: number[] = [1, 2, 3];
 let truths: boolean[] = [true, true, false];
 
 // Classes
-class Car {
-  
-}
+class Car {};
 
 let car: Car = new Car();
 
@@ -23,9 +21,15 @@ let car: Car = new Car();
 let point: { x: number; y: number } = {
   x: 10,
   y: 20
-}
+};
 
 // Function
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use annotations
+// 1) Function that returns the 'any' type
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // {x: 10, y: 20}
