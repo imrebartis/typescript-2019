2df14ee07ac8ea25744332d6d893e41909963756
Imre Bartis <imre.bartis@siili.com>
2019-07-06 05:17:51 +0300

    use annotations when variable's type cannot be inferred correctly

--- fetchjson/variables.ts
+++ fetchjson/variables.ts
@@ -40,7 +40,17 @@ let words  = ['red', 'green', 'blue'];
 let foundWord: boolean;
 
 for (let i = 0; i < words.length; i++) {
-  if(words[i] === 'green') {
+  if (words[i] === 'green') {
     foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero : boolean | number = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
   }
 }
