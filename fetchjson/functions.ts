c2b73f4774118f0d5549afb1aaadc19b9410f3a1
Imre Bartis <imre.bartis@siili.com>
2019-07-11 12:40:35 +0300

    use void and never

--- fetchjson/functions.ts
+++ fetchjson/functions.ts
@@ -1,3 +1,19 @@
 const add = (a: number, b: number): number => {
   return a+b;
 };
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function(a: number, b: number): number {
+  return a * b;
+}
+
+const logger = (message: string): void => {
+  console.log(message);
+}
+
+const throwError = (message: string): never => { // never means we won't execute the function completely
+  throw new Error(message);
+}
