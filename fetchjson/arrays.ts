25b6a3f0d30494edb381ddf106f264af88649bd5
Imre Bartis <imre.bartis@siili.com>
2019-07-18 08:53:59 +0300

    use annotations with arrays

---
+++ fetchjson/arrays.ts
@@ -0,0 +1,13 @@
+const carMakers = ['datsun', 'trabant', 'lada'];
+const someArray: string[] = [];
+
+const dates = [new Date(), new Date()];
+
+const moskovitches = [
+  ['moskovitch1950'],
+  ['moskovitch1960'],
+  ['moskovitch1970']
+];
+
+const twoDimensionalArray: string[][] = [];
+
