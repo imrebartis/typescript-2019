6a61a3dd7e7ac98c032f4acdba373f99c0b16b0d
Imre Bartis <imre.bartis@siili.com>
2019-07-20 14:57:12 +0300

    when to use annotations with arrays

--- fetchjson/arrays.ts
+++ fetchjson/arrays.ts
@@ -11,3 +11,15 @@ const moskovitches = [
 
 const twoDimensionalArray: string[][] = [];
 
+// help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// prevent incompatible values
+carMakers.push(100);
+
+// help with 'map'
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+})
+
