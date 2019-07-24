ea3194a753e3539d0f5186f2adf14d04f853b6d6
Imre Bartis <imre.bartis@siili.com>
2019-07-24 06:56:45 +0300

    use annotations with tuples

---
+++ fetchjson/tuples.ts
@@ -0,0 +1,11 @@
+const drink = {
+  color: 'brown',
+  carbonated: 'true',
+  sugar: 40
+}
+
+// type alias
+type Drink = [string, boolean, number];
+
+const cico: Drink = ['brown', true, 40];
+const frucola: Drink = ['grey', true, 70];
