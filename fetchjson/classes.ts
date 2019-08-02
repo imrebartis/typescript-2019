6331e61c7eb830bb325ca0fd96961a529e8cd468
Imre Bartis <imre.bartis@siili.com>
2019-08-02 06:58:33 +0300

    use instance method modifiers

--- fetchjson/classes.ts
+++ fetchjson/classes.ts
@@ -1,19 +1,20 @@
 class Vehicle {
-  drive(): void {
-    console.log('boom');
-  }
-
-  honk(): void {
+  protected honk(): void {
     console.log('beep');
   }
 }
 
 class Car extends Vehicle {
-  drive(): void {
+  private drive(): void {
     console.log('vroom');
+  }
+
+  public startDrivingProcess(): void {
+    this.drive();
+    this.honk();
   }
 }
 
 const car = new Car;
-car.drive();
-car.honk();
+car.startDrivingProcess();
+car.honk(); // no can do: honk() can be used only in Vehicle or its subclasses, since it's protected & not public
