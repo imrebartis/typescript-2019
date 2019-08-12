9e9a5f9ebd495f205efbc29a24f752a8115d56ff
Imre Bartis <imre.bartis@siili.com>
2019-08-12 06:59:46 +0300

    use type definition files

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -4,10 +4,14 @@ class User {
   name: string;
   location: {
     lat: number;
-    long: number;
+    lng: number;
   };
 
   constructor() {
-
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    }
   }
 }
