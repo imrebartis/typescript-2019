ca9b3539995fc398efe7ce20af7c60a127fa1c58
Imre Bartis <imre.bartis@siili.com>
2019-11-11 16:11:10 +0200

    build the reporter

--- stats/src/Summary.ts
+++ stats/src/Summary.ts
@@ -12,4 +12,9 @@ export class Summary {
   constructor(
     public analyzer: Analyzer,
     public outputTarget: OutputTarget) {}
+  
+  buildAndPrintReport(matches: MatchData[]): void {
+    const output = this.analyzer.run(matches);
+    this.outputTarget.print(output);
+  }
 }
