f452ca777ba1171aeb878114f6af03750e6beefd
Imre Bartis <imre.bartis@siili.com>
2019-11-28 15:58:22 +0200

    finishing touches

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,9 +1,17 @@
 import { MatchReader } from './MatchReader';
 import { CsvFileReader } from './CsvFileReader';
-import { MatchResult } from "./MatchResult";
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
 
 // create an object that satisfies the 'DataReader' interface
 const csvFileReader = new CsvFileReader("football.csv");
 // create an instance of MatchReader and pass in something satisfying the 'DataReader' interface
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
+
+const summary = new Summary(
+  new WinsAnalysis('Everton'),
+  new ConsoleReport()
+)
+summary.buildAndPrintReport(matchReader.matches)
