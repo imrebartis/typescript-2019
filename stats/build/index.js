164f145e67027a1beb0dbd74c8c35355dfb0465f
Imre Bartis <imre.bartis@siili.com>
2019-12-01 10:13:53 +0200

    use a static class method to generate a pre-configured instance of MatchReader

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,12 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
-var CsvFileReader_1 = require("./CsvFileReader");
 var Summary_1 = require("./Summary");
-// create an object that satisfies the 'DataReader' interface
-var csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
-// create an instance of MatchReader and pass in something satisfying the 'DataReader' interface
-var matchReader = new MatchReader_1.MatchReader(csvFileReader);
+var matchReader = MatchReader_1.MatchReader.fromCsv("football.csv");
 matchReader.load();
-var summary = Summary_1.Summary.winsAnalysisWithHtmlReport("Leicester");
+var summary = Summary_1.Summary.winsAnalysisWithHtmlReport("Southampton");
 summary.buildAndPrintReport(matchReader.matches);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,15 +1,9 @@
 import { MatchReader } from "./MatchReader";
-import { CsvFileReader } from "./CsvFileReader";
-import { ConsoleReport } from "./reportTargets/ConsoleReport";
-import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { Summary } from "./Summary";
-import { HtmlReport } from "./reportTargets/HtmlReport";
 
-// create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader("football.csv");
-// create an instance of MatchReader and pass in something satisfying the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv("football.csv");
+const summary = Summary.winsAnalysisWithHtmlReport("Southampton");
+
 matchReader.load();
 
-const summary = Summary.winsAnalysisWithHtmlReport("Leicester");
 summary.buildAndPrintReport(matchReader.matches);
