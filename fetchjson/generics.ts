8234a8df016fdb93a94cc313acec197a915390ca
Imre Bartis <imre.bartis@siili.com>
2019-12-28 11:42:03 +0200

    generic constraints

--- fetchjson/generics.ts
+++ fetchjson/generics.ts
@@ -2,7 +2,7 @@ class ArrayOfNumbers {
   constructor(public collection: number[]) {}
 
   get(index: number): number {
-    return  this.collection[index];
+    return this.collection[index];
   }
 }
 
@@ -10,7 +10,7 @@ class ArrayOfStrings {
   constructor(public collection: string[]) {}
 
   get(index: number): string {
-    return  this.collection[index];
+    return this.collection[index];
   }
 }
 
@@ -18,11 +18,11 @@ class ArrayOfAnything<T> {
   constructor(public collection: T[]) {}
 
   get(index: number): T {
-    return  this.collection[index];
+    return this.collection[index];
   }
 }
 
-new ArrayOfAnything<string>(['a', 'b']);
+new ArrayOfAnything<string>(["a", "b"]);
 
 // example of generics with functions
 function printStrings(arr: string[]): void {
@@ -43,5 +43,31 @@ function printAnything<T>(arr: T[]): void {
   }
 }
 
-printAnything<string>(['a', 'b', 'c']);
+printAnything<string>(["a", "b", "c"]);
 printAnything<number>([1, 2, 3]);
+
+// generic constraints
+class Bar {
+  print() {
+    console.log("I am a bar");
+  }
+}
+
+class Foo {
+  print() {
+    console.log("I am a foo");
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printFoosOrBars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printFoosOrBars<Foo>([new Foo(), new Foo()]);
+printFoosOrBars<Bar>([new Bar(), new Bar()]);
