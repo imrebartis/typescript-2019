4eefca1365bc8651e3dab2ec26ae23ef45e47cff
Imre Bartis <imre.bartis@siili.com>
2020-01-12 03:43:21 +0200

    optional interface properties

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,5 +2,7 @@ import { User } from "./models/User";
 
 const user = new User({ name: "ho", age: 3 });
 
+user.set({ name: "newname" })
+
 console.log(user.get('name'));
 console.log(user.get('age'));
